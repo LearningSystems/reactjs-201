108ee0f61c06e1f4d3207316afaa7646a9fb0a00
Sreedhar Bukya <sreedhar.bukya@toppr.com>
2020-05-13 11:34:44 +0800

    added in progress code.

---
+++ src/Person/Person.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+class Person extends Component {
+    render() {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
+
+export default Person;
